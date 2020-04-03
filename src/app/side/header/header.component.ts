805cfe11b30ac6929341a7868b9d9792a4b9843a
Andrei Voicu <dreivoicu@gmail.com>
2020-04-04 00:50:03 +0300

    adding chats

--- src/app/side/header/header.component.ts
+++ src/app/side/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-side-header',
+  selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
--- src/app/side/side.module.ts
+++ src/app/side/side.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SideComponent } from './side.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
+import { ChatsComponent } from './chats/chats.component';
 
 @NgModule({
   declarations: [
     SideComponent,
     HeaderComponent,
-    SearchComponent
+    SearchComponent,
+    ChatsComponent
   ],
   imports: [
     CommonModule
