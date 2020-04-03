58e77e4764fc2185c81e2aa38cc1eb3f490dac0e
Andrei Voicu <dreivoicu@gmail.com>
2020-04-03 20:53:57 +0300

    side header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @HostBinding('class.main-grid') isMain = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideComponent } from './side/side.component';
+import { ContentComponent } from './content/content.component';
+import { InfoComponent } from './info/info.component';
+import { HeaderComponent } from './side/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideComponent,
+    ContentComponent,
+    InfoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
