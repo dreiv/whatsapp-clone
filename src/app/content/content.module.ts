c8035bfd3636536ccc83aabd0c7828e171c29935
Andrei Voicu <dreivoicu@gmail.com>
2020-04-05 17:23:59 +0300

    added messages

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content.component';
+import { MessangerComponent } from './messanger/messanger.component';
 
 @NgModule({
-  declarations: [HeaderComponent, ContentComponent],
+  declarations: [HeaderComponent, ContentComponent, MessangerComponent],
   imports: [
     CommonModule
   ],
---
+++ src/app/content/messanger/messanger.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-messanger',
+  templateUrl: './messanger.component.html',
+  styleUrls: ['./messanger.component.scss']
+})
+export class MessangerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
